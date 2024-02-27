b183e6c6d0bf361c4cbef5661908566c56f23cfe
Matheus Santos <matheus.slima@sptech.school>
2024-02-27 17:19:43 -0300

    Criacao do repo da api

---
+++ pitstop_api/src/main/java/org/example/pitstop_api/PitstopApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.pitstop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitstopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitstopApiApplication.class, args);
+    }
+
+}
