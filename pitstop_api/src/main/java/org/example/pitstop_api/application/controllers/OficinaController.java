40778bd45fb74d921d6ad797ac674d788fee778c
KauaJuhrs <kaua.costa@sptech.school>
2024-03-02 17:23:07 -0300

    criando controladores

---
+++ pitstop_api/src/main/java/org/example/pitstop_api/application/controllers/OficinaController.java
@@ -0,0 +1,9 @@
+package org.example.pitstop_api.application.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oficina")
+public class OficinaController {
+}
