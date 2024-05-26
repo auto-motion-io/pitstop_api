a45475f37d730a13aede7026c643976680d8dd97
Leopls <leonardobento233@gmail.com>
2024-05-25 21:34:55 -0300

    removendo o deletedAt do cliente @JsonIgnore

--- src/main/java/org/motion/motion_api/domain/entities/pitstop/Cliente.java
+++ src/main/java/org/motion/motion_api/domain/entities/pitstop/Cliente.java
@@ -21,6 +21,7 @@ public class Cliente {
     private String nome;
     private String telefone;
     private String email;
+    @JsonIgnore
     private LocalDate deletedAt;
 
     @ManyToOne @JoinColumn(name = "fkOficina")
